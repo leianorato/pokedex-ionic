d1f2ccfdbbf5eff2beaa1afa2b131265c1423b0c
Wanisleia Norato <leianorato@gmail.com>
2024-04-11 14:02:46 -0300

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pokedex',
+  webDir: 'www',
+  bundledWebRuntime: false
+  // server: {
+  //   androidScheme: 'https'
+  // }
+};
+
+export default config;
---
+++ src/app/app.routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from '@angular/router';
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((c) =>
+      c.HomePage)
+  },
+  {
+    path: 'detalhes',
+    loadComponent: () => import('./detalhes/detalhes.page').then( m => m.DetalhesPage)
+  },
+  {
+    path: 'detalhes/:id',
+    loadComponent: () => import('./detalhes/detalhes.page').then((c) =>
+      c.DetalhesPage)
+  }
+];
